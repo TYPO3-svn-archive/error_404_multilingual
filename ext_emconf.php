ee5311c27a5d6e9f5f9c33ea2c82501bfb21ea0b
juergen.furrer <juergen.furrer@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-08-02 21:44:40 +0000

    Now works with newer Typo3 Version
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/error_404_multilingual/trunk@50441 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "error_404_multilingual".
 #
-# Auto generated 16-10-2010 13:09
+# Auto generated 02-08-2011 23:44
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Shows the defined error page of the given language if the requested page or file could not be found',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '0.2.9',
+	'version' => '0.3.0',
 	'dependencies' => 'realurl',
 	'conflicts' => 'error_404_handling',
 	'priority' => '',
@@ -36,7 +36,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'depends' => array(
 			'realurl' => '',
 			'php' => '5.0.0-0.0.0',
-			'typo3' => '4.0.0-0.0.0',
+			'typo3' => '4.0.0-4.99.999',
 		),
 		'conflicts' => array(
 			'error_404_handling' => '',
@@ -44,7 +44,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:4:{s:21:"class.ux_tslib_fe.php";s:4:"900f";s:12:"ext_icon.gif";s:4:"b4a8";s:17:"ext_localconf.php";s:4:"5f36";s:14:"doc/manual.sxw";s:4:"fb7b";}',
+	'_md5_values_when_last_written' => 'a:4:{s:21:"class.ux_tslib_fe.php";s:4:"900f";s:12:"ext_icon.gif";s:4:"b4a8";s:17:"ext_localconf.php";s:4:"4fc0";s:14:"doc/manual.sxw";s:4:"75c1";}',
 	'suggests' => array(
 	),
 );
