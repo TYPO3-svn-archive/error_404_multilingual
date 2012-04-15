611e81545f575ab1ec53194ecdc3030a20696589
juergen.furrer <juergen.furrer@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-04-15 20:27:27 +0000

    Remove CURLOPT_FOLLOWLOCATION to remove Log-Entries if open_basedir is used
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/error_404_multilingual/trunk@60750 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "error_404_multilingual".
 #
-# Auto generated 29-12-2011 23:04
+# Auto generated 15-04-2012 22:27
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Shows the defined error page of the given language if the requested page or file could not be found',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '0.3.1',
+	'version' => '0.3.2',
 	'dependencies' => 'realurl',
 	'conflicts' => 'error_404_handling',
 	'priority' => '',
@@ -44,7 +44,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:4:{s:21:"class.ux_tslib_fe.php";s:4:"80e0";s:12:"ext_icon.gif";s:4:"b4a8";s:17:"ext_localconf.php";s:4:"4fc0";s:14:"doc/manual.sxw";s:4:"ae44";}',
+	'_md5_values_when_last_written' => 'a:4:{s:21:"class.ux_tslib_fe.php";s:4:"293f";s:12:"ext_icon.gif";s:4:"b4a8";s:17:"ext_localconf.php";s:4:"4fc0";s:14:"doc/manual.sxw";s:4:"e6e8";}',
 	'suggests' => array(
 	),
 );
